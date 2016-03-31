186e63abf5fb3e1c2dbd26b23f123139d0e1c7c1
sunghuna <sunghuna63@gmail.com>
2016-03-31 19:28:40 +0900

    github test222

--- app/src/main/java/com/example/sunghuncom/hw_1_a_0327/MainActivity.java
+++ app/src/main/java/com/example/sunghuncom/hw_1_a_0327/MainActivity.java
@@ -6,7 +6,7 @@ package com.example.sunghuncom.hw_1_a_0327;
  //  Created by Kwak sunghun on 2016. 3. 27.
  //  Copyright © 2016년 Kwak sunghun. All rights reserved.
  */
-//github test2222
+//github test2222122
 import android.util.Log;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
